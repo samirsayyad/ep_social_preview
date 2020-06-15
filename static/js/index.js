30ab8ee87bdcc11b73db94d634885e5bc8ed6e29
Samir Sayyad <samir.saiad@gmail.com>
2020-06-15 18:03:32 +0430

    found title and meta has been generated

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,10 @@
 exports.handleClientMessage_CUSTOM = function(hook, context, cb){
-  console.log("handleHeaderTitleMessage has been runned")
   if(context.payload.action == "recieveTitleMessage"){
     var message = context.payload.message;
-    if(!$("#input_title").is(":visible")){ // if we're not editing..
-      if(message){
-        $('head').append('  <meta property="og:title" content="'+message+' " />');
-      }
+    if(message){
+      $('head').append('<meta property="og:title" content="'+message+'" />');
+    }else{
+      $('head').append('<meta property="og:title" content="Untitle Document" />');
     }
   }
 }
