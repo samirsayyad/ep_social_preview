767f807760638b58d825d3b067031ebc2d4f127c
Samir Sayyad <samir.saiad@gmail.com>
2020-06-15 16:25:56 +0430

    trying get the title

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-var settings = require('ep_etherpad-lite/node/utils/Settings'),
-          db = require('ep_etherpad-lite/node/db/DB').db;
-
-// Remove cache for this procedure
-db['dbSettings'].cache = 0;
-
-exports.exportFileName = function(hook, padId, callback){
-  var title = padId;
-  // Sets Export File Name to the same as the title
-  db.get("title:"+padId, function(err, value){
-    console.log("ep_social_preview: Found ", value, " for ", padId);
-    if(value) title = value;
-  });
-  callback(title);
-}
+exports.handleTitle = function(hook, context, cb){
+    if(context.payload.action == "recieveTitleMessage"){
+      var message = context.payload.message;
+        if(message){
+            alert(message)
+        }
+      
+    }
+  }
