7b79b9b1f06e83f6ff4e134e406ebbf29b79df94
Samir Sayyad <samir.saiad@gmail.com>
2020-06-15 17:58:22 +0430

    trying get the title

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,11 @@
-exports.handleTitle = function(hook, context, cb){
-    if(context.payload.action == "recieveTitleMessage"){
-      var message = context.payload.message;
-        if(message){
-            $('head').append('  <meta property="og:title" content="aaaaaaaaaaaaaaa " />');
-
-        }
-      
+exports.handleClientMessage_CUSTOM = function(hook, context, cb){
+  console.log("handleHeaderTitleMessage has been runned")
+  if(context.payload.action == "recieveTitleMessage"){
+    var message = context.payload.message;
+    if(!$("#input_title").is(":visible")){ // if we're not editing..
+      if(message){
+        $('head').append('  <meta property="og:title" content="'+message+' " />');
+      }
     }
   }
+}
